61514b35f8b343c66287da598101d892bcb83fc1
Kristian Glass <git@doismellburning.co.uk>
2014-04-02 14:27:25 +0100

    Add classifiers for Python version support

--- setup.py
+++ setup.py
@@ -36,5 +36,9 @@ setup(
     classifiers=(
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
     ),
 )
