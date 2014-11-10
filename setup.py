5155a194e09618e7c95f4f9f69e17927a30f107b
Kristian Glass <git@doismellburning.co.uk>
2014-11-10 11:24:18 +0000

    Add Python 3.4 classifier

--- setup.py
+++ setup.py
@@ -45,6 +45,7 @@ setup(
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
     ),
 
 )
