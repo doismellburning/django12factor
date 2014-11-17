483e4c24ba7688e2f60964c50e8dcafee76af12e
Kristian Glass <git@doismellburning.co.uk>
2014-11-17 14:36:33 +0000

    Ensure that logs are going to stdout (instead of stderr as previously!)
    Alas this test doesn't actually work if you fix the stdout handler (but
    nose seems to capture the stdout appropriately), but hey, progress...

--- tests/test_logging.py
+++ tests/test_logging.py
@@ -1,8 +1,13 @@
-from __future__ import absolute_import
+from __future__ import (
+    absolute_import,
+    print_function,
+)
 
+import StringIO
 import django12factor
 import logging
 import logging.config
+import sys
 import unittest
 
 from .env import env
@@ -12,6 +17,19 @@ debug_env = env(DEBUG="1", SECRET_KEY="secret")
 
 def has_handler(logger, handler_name):
     return any([handler.name == handler_name for handler in logger.handlers])
+
+
+def capture_stdout(command, *args, **kwargs):
+    normal_stdout = sys.stdout
+    captured_stdout = StringIO.StringIO()
+    sys.stdout = captured_stdout
+
+    command(*args, **kwargs)
+
+    captured_stdout.seek(0)
+    return captured_stdout.read()
+
+    sys.stdout = normal_stdout
 
 
 class TestLogging(unittest.TestCase):
@@ -31,3 +49,15 @@ class TestLogging(unittest.TestCase):
         with debug_env:
             logging.config.dictConfig(django12factor.factorise()['LOGGING'])
             self.assertTrue(has_handler(logging.root, "stdout"))
+
+    def test_logging_to_stdout(self):
+
+        with debug_env:
+            logging.config.dictConfig(django12factor.factorise()['LOGGING'])
+
+            output = capture_stdout(print, "wibble")
+            self.assertIn("wibble", output)
+
+            message = "lorem ipsum"
+            output = capture_stdout(logging.info, message)
+            self.assertIn(message, output, "Message '%s' should have been logged to stdout and captured; instead '%s' was captured" % (message, output))
