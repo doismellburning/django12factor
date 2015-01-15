3b3538db7329a88b6e9ceadfae876cbf85b4fa8a
Kristian Glass <git@doismellburning.co.uk>
2015-01-15 16:06:49 +0000

    Extract out `debugenv` for less copypasta

--- tests/test_d12f.py
+++ tests/test_d12f.py
@@ -8,6 +8,11 @@ from .env import env
 
 d12f = django12factor.factorise
 
+
+def debugenv(**kwargs):
+    return env(DEBUG="true", **kwargs)
+
+
 class TestD12F(unittest.TestCase):
 
     def test_object_no_secret_key_prod(self):
@@ -15,29 +20,29 @@ class TestD12F(unittest.TestCase):
             self.assertRaises(SystemExit, d12f)
 
     def test_debug(self):
-        with env(DEBUG="true"):
+        with debugenv():
             self.assertTrue(d12f()['DEBUG'])
 
     def test_db(self):
-        with env(DEBUG="true"):
+        with debugenv():
             self.assertIn("sqlite", d12f()['DATABASES']['default']['ENGINE'])
 
-        with env(DEBUG="true", DATABASE_URL="sqlite://:memory:"):
+        with debugenv(DATABASE_URL="sqlite://:memory:"):
             self.assertIn("sqlite", d12f()['DATABASES']['default']['ENGINE'])
 
-        with env(DEBUG="true", DATABASE_URL="postgres://username:password@host:1234/dbname"):
+        with debugenv(DATABASE_URL="postgres://username:password@host:1234/dbname"):
             db = d12f()['DATABASES']['default']
             self.assertIn("postgres", db['ENGINE'])
             self.assertEquals("dbname", db['NAME'])
 
     def test_custom_key(self):
-        with env(DEBUG="true", CUSTOM_KEY="banana"):
+        with debugenv(CUSTOM_KEY="banana"):
             settings = d12f(['CUSTOM_KEY'])
             self.assertIn("banana", settings['CUSTOM_KEY'])
 
     def test_missing_custom_keys(self):
         present = 1
-        with env(DEBUG="true", PRESENT=present):
+        with debugenv(PRESENT=present):
             settings = d12f(['PRESENT', 'MISSING'])
             self.assertEquals(present, settings['PRESENT'])
             self.assertIsNone(settings['MISSING'])
