8901cb43c5629fd6d199216e2ed66beb88cbe3fc
Kristian Glass <git@doismellburning.co.uk>
2015-01-16 00:05:18 +0000

    Custom default value

--- django12factor/environment_variable_loader.py
+++ django12factor/environment_variable_loader.py
@@ -2,11 +2,12 @@ import os
 
 
 class EnvironmentVariableLoader(object):
-    def __init__(self, name, **kwargs):
+    def __init__(self, name, default=None, **kwargs):
         self.name = name
+        self.default = default
 
     def load_value(self):
-        return os.getenv(self.name)
+        return os.getenv(self.name, default=self.default)
 
 
 EVL = EnvironmentVariableLoader
--- tests/test_evl.py
+++ tests/test_evl.py
@@ -19,3 +19,26 @@ class TestEVL(unittest.TestCase):
             custom_evl = d12f(custom_settings=[EVL("FOO")])
 
         self.assertEqual(custom_string, custom_evl)
+
+    def test_default_is_none(self):
+        with debugenv():
+            key = "FOO"
+            d = d12f(custom_settings=[EVL(key)])
+            self.assertIsNone(
+                d[key],
+                "d12f tried to load the (unset) environment variable '%s' "
+                "with no default; expected None but got %s instead" %
+                (key, str(d[key]))
+            )
+
+    def test_custom_default(self):
+        with debugenv():
+            key = "FOO"
+            default = "llama"
+            d = d12f(custom_settings=[EVL(key, default=default)])
+            self.assertEquals(
+                default, d[key],
+                "d12f tried to load the (unset) environment variable '%s' "
+                "with a default of %s, but got %s instead" %
+                (key, str(default), str(d[key]))
+            )
