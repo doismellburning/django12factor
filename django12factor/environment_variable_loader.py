14404f017d5b99a54fced1b4a9d440f0dd6855db
Kristian Glass <git@doismellburning.co.uk>
2015-01-16 09:40:28 +0000

    Drop unused **kwargs argument

--- django12factor/environment_variable_loader.py
+++ django12factor/environment_variable_loader.py
@@ -2,7 +2,7 @@ import os
 
 
 class EnvironmentVariableLoader(object):
-    def __init__(self, name, default=None, **kwargs):
+    def __init__(self, name, default=None):
         self.name = name
         self.default = default
 
