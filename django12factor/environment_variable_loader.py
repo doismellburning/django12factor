9ac7e4b16188e46e903724c9f965a27906dfe156
Kristian Glass <git@doismellburning.co.uk>
2015-01-17 01:18:30 +0000

    Documentation for EVL

--- django12factor/environment_variable_loader.py
+++ django12factor/environment_variable_loader.py
@@ -6,6 +6,19 @@ identity = lambda x: x
 
 class EnvironmentVariableLoader(object):
     def __init__(self, name, default=None, parser=identity):
+        """
+        Builds a loader for an environment variable.
+
+        * `name`: (required) the environment key to use
+        * `default`: a value to use if the environment variable is not set
+        * `loader`: a function that takes a string environment value and
+          performs type conversion / parsing / etc. (e.g. `int` or
+          `lambda x: x.split(',')`)
+
+        Note there is no checking to ensure the type of `default` is the same
+        as the return type of `loader`, but if this isn't the case, you're
+        probably going to have a sad time.
+        """
         self.name = name
         self.default = default
         self.parser = parser
