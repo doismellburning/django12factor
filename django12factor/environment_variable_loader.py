5fb1b08ee5dcf265bbc1e7430c404bad5d269309
Kristian Glass <git@doismellburning.co.uk>
2015-03-09 00:08:57 +0000

    Docstrings to plcate pep257

--- django12factor/environment_variable_loader.py
+++ django12factor/environment_variable_loader.py
@@ -1,14 +1,28 @@
+"""
+Utility class for transforming environment variables into Django settings.
+"""
+
 import os
 
 
 def identity(x):
+    """
+    Because Python's stdlib doesn't have an identity function.
+    """
+
     return x
 
 
 class EnvironmentVariableLoader(object):
+    """
+    Load an environment variable, for a flexible value of load.
+
+    Provide an (optional) parser function (e.g. `int`), and an (optional) default.
+    """
+
     def __init__(self, name, default=None, parser=identity):
         """
-        Builds a loader for an environment variable.
+        Build a loader for an environment variable.
 
         * `name`: (required) the environment key to use
         * `default`: a value to use if the environment variable is not set
@@ -20,11 +34,16 @@ class EnvironmentVariableLoader(object):
         as the return type of `loader`, but if this isn't the case, you're
         probably going to have a sad time.
         """
+
         self.name = name
         self.default = default
         self.parser = parser
 
     def load_value(self):
+        """
+        Load and process an environment variable.
+        """
+
         if self.name in os.environ:
             return self.parser(os.environ[self.name])
         else:
