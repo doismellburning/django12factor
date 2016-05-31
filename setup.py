4bca5bf9bd722b3fb7bf3278ce87346574af20b4
requires.io <support@requires.io>
2016-05-31 19:03:12 +0100

    [requires.io] dependency update

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
 
     install_requires=(
         "dj-database-url==0.4.1",
-        "dj-email-url==0.0.6",
+        "dj-email-url==0.0.7",
         "django-cache-url==1.0.0",
         "six",
     ),
