4cccd232541e0c57c631e0bcee93892188485644
Jonas Maurus <jonas-github@maurus.net>
2016-09-14 12:02:04 +0200

    add pep484 stub file for type checking

---
+++ django12factor/__init__.pyi
@@ -0,0 +1,6 @@
+# -* encoding: utf-8 *-
+from typing import Dict, Any, Sequence
+
+
+def getenv_bool(setting_name: str) -> bool: ...
+def factorise(custom_settings: Sequence[str]=None) -> Dict[str, Any]: ...
