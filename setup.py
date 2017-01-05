2a229639b39d18af5acbd297b0a875d7f0ea0c2d
requires.io <support@requires.io>
2017-01-05 18:17:00 +0000

    [requires.io] dependency update

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
     keywords='django 12factor configuration',
 
     install_requires=(
-        "dj-database-url==0.4.1",
+        "dj-database-url==0.4.2",
         "dj-email-url==0.0.10",
         "django-cache-url==1.3.1",
         "six",
